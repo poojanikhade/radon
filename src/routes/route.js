6bc94bc5e97cf82f40e7fe638ea5864ad0c77a57
Pooja <poojanikhade2@gmail.com>
2022-06-04 17:09:45 +0530

    git api assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -44,11 +44,11 @@ var result = lodash.fromPairs(kvPairs);
 const router = express.Router();
   
 router.get('/hello',function (req,res){
-    splitIntoChunk(array, chunk);
-    console.log(odds)
-    console.log(newArray);
-    console.log(merge);
-    console.log(kvPairs);
+    // splitIntoChunk(array, chunk);
+    // console.log(odds)
+    // console.log(newArray);
+    // console.log(merge);
+    // console.log(kvPairs);
      //console.log('The constant in logger route has a value '+externalModule.endpoint)
     //console.log('The current batch is '+externalModule.batch)
     //externalModule.welcome()
@@ -60,11 +60,49 @@ router.get('/hello',function (req,res){
     //externalModule.toUpperCase()
 
    res.send('My first ever api!')
-})
+});
+    
+  
+// write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+   router.get("/sol1", function (req, res) {
+  //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+  let arr= [1,2,3,5,6,7]
+
+  let totalsum = 0;
+  for (var i in arr) {
+      totalsum += arr[i];
+  }
+  let lastDigit= arr.pop()
+  let consecutiveSum= lastDigit * (lastDigit+1) / 2
+  let missingNumber= consecutiveSum - totalsum
+
+  res.send(  { data: missingNumber  }  );
+});
+
+//write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+router.get("/sol2", function (req, res) {
+  //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+  let arr= [33, 34, 35, 37, 38]
+  let len= arr.length
+
+  let totalsum = 0;
+  for (var i in arr) {
+      totalsum += arr[i];
+  }
+  let firstDigit= arr[0]
+  let lastDigit= arr.pop()
+  let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+  let missingNumber= consecutiveSum - totalsum
  
+  res.send(  { data: missingNumber  }  );
+});
 
 
-module.exports = router;
+
+
+
+
+   module.exports = router;
 // adding this comment for no reason 
 
 
