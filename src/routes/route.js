b2000eae59069bb595e1768adb1bbecf7165c9bb
Pooja <poojanikhade2@gmail.com>
2022-06-06 09:09:38 +0530

    assignment2/get-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const router = express.Router();
+const app = express.Router();
   
 // write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
-   router.get("/sol1", function (req, res) {
+   app.get("/sol1", function (req, res) {
   //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
   let arr= [1,2,3,5,6,7]
 
@@ -19,7 +19,7 @@ const router = express.Router();
 });
 
 //write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
-router.get("/sol2", function (req, res) {
+app.get("/sol2", function (req, res) {
   //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
   let arr= [33, 34, 35, 37, 38]
   let len= arr.length
@@ -41,7 +41,7 @@ router.get("/sol2", function (req, res) {
 
 
 
-   module.exports = router;
+   module.exports = app;
 // adding this comment for no reason 
 
 
